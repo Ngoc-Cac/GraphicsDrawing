0bcaa5e53a4897209868da6fc379e693f5a1622b
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-06-08 16:05:37 +0700

    Create colormap_maker.py

---
+++ Mandelbrot's Set/colormap_maker.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from scipy.interpolate import interp1d
+
+
+def make_gradient(colors, interpolation="linear"):
+    X = [i / (len(colors) - 1) for i in range(len(colors))]
+    Y = [[color[i] for color in colors] for i in range(3)]
+    channels = [interp1d(X, y, kind=interpolation) for y in Y]
+    return lambda x: [np.clip(channel(x), 0, 1) for channel in channels]
