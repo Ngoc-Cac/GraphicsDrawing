017ed998fe8026b3b6c82776b496e57c2513f0c9
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-06-14 15:56:31 +0700

    make class non-static

--- Lindenmayer/L_sys/base.py
+++ Lindenmayer/L_sys/base.py
@@ -3,51 +3,87 @@ import math
 from tqdm import tqdm
 
 
+def _move_turtle(
+    start_pos: tuple[float, float],
+    heading: float,
+    length: float = 1
+):
+    radians = math.radians(heading)
+    return (
+        round(start_pos[0] + math.cos(radians) * length, 10),
+        round(start_pos[1] + math.sin(radians) * length, 10),
+    )
+
 class LSystem:
-    rules=None
-    turning_angle=90
-    initial_state=None
-    shrink_factor=1
+    __slots__='_rules', '_initial_state', '_turning_angle', '_shrink_factor'
     _moving_actions=set('FG')
 
-    @classmethod
-    def _apply_rule(cls,
-        state: str
+    def __init__(self,
+        rules: dict[str, str] | None = None,
+        initial_state: str | None = None,
+        turning_angle: int | float = 90,
+        shrink_factor: int | float = 1,
     ):
+        if not isinstance(rules, dict):
+            raise TypeError('rules must be a dict')
+        if not isinstance(initial_state, str):
+            raise TypeError('inital_state must be a string')
+        
+        self._rules = rules
+        self._initial_state = initial_state
+        self.turning_angle = turning_angle
+        self.shrink_factor = shrink_factor
+
+    @property
+    def rules(self) -> dict[str, str]:
+        return dict(self._rules)
+    @property
+    def initial_state(self) -> str:
+        return self._initial_state
+    
+    @property
+    def turning_angle(self) -> int | float:
+        return self._turning_angle
+    @turning_angle.setter
+    def turning_angle(self, angle: int | float):
+        if not isinstance(angle, int | float):
+            raise TypeError('turning_angle must be a number in degrees')
+        self._turning_angle = angle
+
+    @property
+    def shrink_factor(self) -> int | float:
+        return self._shrink_factor
+    @shrink_factor.setter
+    def shrink_factor(self, shrink_factor: int | float):
+        if not isinstance(shrink_factor, int | float):
+            raise TypeError('shrink_factor must be a positive number')
+        elif shrink_factor <= 0:
+            raise ValueError('shrink_factor must be a positive number')
+        self._shrink_factor = shrink_factor
+
+
+    def _apply_rule(self, state: str):
         return ''.join([
-            cls.rules[char]
-            if char in cls.rules else char
+            self.rules[char]
+            if char in self.rules else char
             for char in state
         ])
-    
-    @classmethod
-    def _move_turtle(cls,
-        start_pos: tuple[float, float],
-        heading: float,
-        length: float = 1
-    ):
-        radians = math.radians(heading)
-        return (
-            round(start_pos[0] + math.cos(radians) * length, 10),
-            round(start_pos[1] + math.sin(radians) * length, 10),
-        )
 
-    @classmethod
-    def grow(cls,
+
+    def grow(self,
         initial_state: str | None = None,
         num_iters: int = 5
     ):
-        if cls.rules is None:
+        if self.rules is None:
             raise NotImplementedError('No rules have been set')
         
         if initial_state is None:
-            initial_state = cls.initial_state
+            initial_state = self.initial_state
         for _ in tqdm(range(num_iters)):
-            initial_state = cls._apply_rule(initial_state)
+            initial_state = self._apply_rule(initial_state)
         return initial_state
 
-    @classmethod
-    def process_state(cls,
+    def process_state(self,
         state: str,
         start_pos: tuple[int, int] = (0, 0),
         heading: float = 90,
@@ -56,8 +92,8 @@ class LSystem:
         stack = []
         xs, ys = [], []
         for action in state:
-            if action in cls._moving_actions:
-                end_pos = cls._move_turtle(start_pos, heading, length)
+            if action in self._moving_actions:
+                end_pos = _move_turtle(start_pos, heading, length)
                 xs.append(start_pos[0])
                 xs.append(end_pos[0])
                 xs.append(None)
@@ -66,10 +102,10 @@ class LSystem:
                 ys.append(None)
 
                 start_pos = end_pos
-            elif action == '+': heading += cls.turning_angle
-            elif action == '-': heading -= cls.turning_angle
-            elif action == '*': length  *= cls.shrink_factor
-            elif action == '/': length  /= cls.shrink_factor
+            elif action == '+': heading += self.turning_angle
+            elif action == '-': heading -= self.turning_angle
+            elif action == '*': length  *= self.shrink_factor
+            elif action == '/': length  /= self.shrink_factor
             elif action == '[':
                 stack.append((start_pos, heading))
             elif action == ']':
